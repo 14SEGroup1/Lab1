9f7811598a811652bfce6565b79a74e535eb7fc0
Qizixi <1115146536@qq.com>
2016-03-18 22:02:02 +0800

    added class OrderMachine to do some of main function's work

--- Lab1/src/lab1/Main.java
+++ Lab1/src/lab1/Main.java
@@ -1,7 +1,7 @@
 package lab1;
 import java.text.DecimalFormat;
 public class Main {
-	private static Beverage order;
+	//private static Beverage order;
 	private static String[] disArr; 
 	public static void main(String[] args) {
 		disArr= new String[args.length]; 
@@ -9,11 +9,20 @@ public class Main {
 			disArr[j] = args[j].toLowerCase(); 
 		} 
 		int i;
-		for (i = 0; i < disArr.length; i++)
-			if (disArr[i].equals("small") || disArr[i].equals("medium")
-					|| disArr[i].equals("large") || disArr[i].equals("grand")){
+		for (i = 0; i < disArr.length; i++){
+			//use switch-case to reduce comparison times
+			int sizeExistance = 1;
+			switch(disArr[i]){
+			    case "small" :sizeExistance = 0;break;
+			    case "medium":sizeExistance = 0;break;
+			    case "large" :sizeExistance = 0;break;
+			    case "grand" :sizeExistance = 0;break;
+			    default :break;
+			}
+			if(sizeExistance==0){
 				break;
 			}
+		}
 		if (i >= disArr.length) {
 			System.out.println("Must set a size!");
 			return;
@@ -25,146 +34,11 @@ public class Main {
 			beveStr = disArr[0];
 		}
 		
-		
-		setSize(beveStr,i);//Create an beverage instance based on the input of the user including it's size 
-		addIngredient(i);//add ingredient to the beverage if urged	
+		OrderMachine ordermachine = new OrderMachine(disArr);
+		Beverage order = ordermachine.getBeverage(beveStr,i);//Create an beverage instance based on the input of the user including it's size 
+		ordermachine.addIngredient(order,i);//add ingredient to the beverage if urged	
 		DecimalFormat df = new DecimalFormat(".0");
-		System.out.println("The total cost of your order is: "
-				+ df.format(order.cost()));
-			
-		
-		/**
-		 * How do I get the description of each order instead of doing this
-		 * stupid thing forever (except for printing the args)?
-		 */
-		if (order instanceof BeverageWithIngredient){
-			((BeverageWithIngredient) order).getDescription();
-		} 
-		else if (order instanceof Espresso) {
-			((Espresso) order).getDescription();
-		} 
-		else if (order instanceof HouseBlend){
-			((HouseBlend) order).getDescription();
-		} 
-		else if (order instanceof Decaf){
-			((Decaf) order).getDescription();
-		} 
-		else if (order instanceof RedTea){
-			((RedTea) order).getDescription();
-		} 
-		else if (order instanceof GreenTea){
-			((GreenTea) order).getDiscription();
-		} 
-		else {
-			((WhiteTea) order).getDescription();
+		System.out.println("The total cost of your order is: "+ df.format(order.cost()));
+		ordermachine.getDescription(order);
 		}
-	}
-	
-	public static void setSize(String beveStr,int i){
-		switch (beveStr) {
-		case "espresso" :
-			order = new Espresso();
-			((CoffeeBeverage) order).setSize(disArr[i]);
-			break;
-		case "houseblend" :
-			order = new HouseBlend();
-			((CoffeeBeverage) order).setSize(disArr[i]);
-			break;
-		case "mocha" :
-			order = new Espresso();
-			((CoffeeBeverage) order).setSize(disArr[i]);
-			order = new Chocolate(order);
-			break;
-		case "latte" : 
-			order = new Espresso();
-			((CoffeeBeverage) order).setSize(disArr[i]);
-			order = new Milk(order);
-			break;
-		case "cappuccino" :
-			order = new Espresso();
-			((CoffeeBeverage) order).setSize(disArr[i]);
-			order = new WhipCream(order);
-			break;
-		case "green tea" :
-			order = new GreenTea();
-			((TeaBeverage) order).setSize(disArr[i]);
-			break;
-		case "red tea" :
-			order = new RedTea();
-			((TeaBeverage) order).setSize(disArr[i]);
-			break;
-		case "white tea" : 
-			order = new WhiteTea();
-			((TeaBeverage) order).setSize(disArr[i]);
-			break;
-		case "flower tea" :
-			order = new GreenTea();
-			((TeaBeverage) order).setSize(disArr[i]);
-			order = new Jasmine(order);
-			break;
-		case "ginger tea" :
-			order = new GreenTea();
-			((TeaBeverage) order).setSize(disArr[i]);
-			order = new Ginger(order);
-			break;
-		case "tea latte" :
-			order = new RedTea();
-			((TeaBeverage) order).setSize(disArr[i]);
-			order = new Milk(order);
-			break;
-		case "decaf mocha" :
-			order = new Decaf();
-			((CoffeeBeverage) order).setSize(disArr[i]);
-			order = new Chocolate(order);
-			break;
-		case "decaf" :
-			order = new Decaf();
-			((CoffeeBeverage) order).setSize(disArr[i]);
-			break;
-		case "decaf latte" :
-			order = new Decaf();
-			((CoffeeBeverage) order).setSize(disArr[i]);
-			order = new Milk(order);
-			break;
-		case "decaf cappuccino" :
-			order = new Decaf();
-			((CoffeeBeverage) order).setSize(disArr[i]);
-			order = new WhipCream(order);
-			break;
-		default :
-			System.out.println("Illegal input: " + beveStr);
-			System.exit(0);
-		}
-	}
-	public static void addIngredient(int i){
-		for (i++; i < disArr.length; i++) {
-			switch( disArr[i] ){
-
-			case "chocolate" :
-				order = new Chocolate(order);
-				break;
-
-			case "ginger" :
-				order = new Ginger(order);
-				break;
-
-			case "milk" :
-				order = new Milk(order);
-				break;
-
-			case "jasmine" :
-				order = new Jasmine(order);
-				break;
-
-			case "whip" :
-				i++;
-				order = new WhipCream(order);
-				break;
-
-			default :
-				System.out.println("Illegal input: " + disArr[i]);
-				System.exit(0);
-			}
-		}
-	}
-}
+  }
---
+++ Lab1/src/lab1/OrderMachine.java
@@ -0,0 +1,151 @@
+package lab1;
+
+public class OrderMachine {
+	
+	private String[] orderMessage;
+	
+	public OrderMachine(String[] inputMessage){
+		orderMessage = inputMessage;
+	}
+	
+	public Beverage getBeverage(String beveStr,int beveSize){
+		Beverage order = new Beverage();
+		switch (beveStr) {
+		case "espresso" :
+			order = new Espresso();
+			((CoffeeBeverage) order).setSize(orderMessage[beveSize]);
+			break;
+		case "houseblend" :
+			order = new HouseBlend();
+			((CoffeeBeverage) order).setSize(orderMessage[beveSize]);
+			break;
+		case "mocha" :
+			order = new Espresso();
+			((CoffeeBeverage) order).setSize(orderMessage[beveSize]);
+			order = new Chocolate(order);
+			break;
+		case "latte" : 
+			order = new Espresso();
+			((CoffeeBeverage) order).setSize(orderMessage[beveSize]);
+			order = new Milk(order);
+			break;
+		case "cappuccino" :
+			order = new Espresso();
+			((CoffeeBeverage) order).setSize(orderMessage[beveSize]);
+			order = new WhipCream(order);
+			break;
+		case "green tea" :
+			order = new GreenTea();
+			((TeaBeverage) order).setSize(orderMessage[beveSize]);
+			break;
+		case "red tea" :
+			order = new RedTea();
+			((TeaBeverage) order).setSize(orderMessage[beveSize]);
+			break;
+		case "white tea" : 
+			order = new WhiteTea();
+			((TeaBeverage) order).setSize(orderMessage[beveSize]);
+			break;
+		case "flower tea" :
+			order = new GreenTea();
+			((TeaBeverage) order).setSize(orderMessage[beveSize]);
+			order = new Jasmine(order);
+			break;
+		case "ginger tea" :
+			order = new GreenTea();
+			((TeaBeverage) order).setSize(orderMessage[beveSize]);
+			order = new Ginger(order);
+			break;
+		case "tea latte" :
+			order = new RedTea();
+			((TeaBeverage) order).setSize(orderMessage[beveSize]);
+			order = new Milk(order);
+			break;
+		case "decaf mocha" :
+			order = new Decaf();
+			((CoffeeBeverage) order).setSize(orderMessage[beveSize]);
+			order = new Chocolate(order);
+			break;
+		case "decaf" :
+			order = new Decaf();
+			((CoffeeBeverage) order).setSize(orderMessage[beveSize]);
+			break;
+		case "decaf latte" :
+			order = new Decaf();
+			((CoffeeBeverage) order).setSize(orderMessage[beveSize]);
+			order = new Milk(order);
+			break;
+		case "decaf cappuccino" :
+			order = new Decaf();
+			((CoffeeBeverage) order).setSize(orderMessage[beveSize]);
+			order = new WhipCream(order);
+			break;
+		default :
+			System.out.println("Illegal input: " + beveStr);
+			System.exit(0);
+		}
+		return order;
+	}
+	
+	public void addIngredient(Beverage order,int i){
+		for (i++; i < orderMessage.length; i++) {
+			switch( orderMessage[i] ){
+
+			case "chocolate" :
+				order = new Chocolate(order);
+				break;
+
+			case "ginger" :
+				order = new Ginger(order);
+				break;
+
+			case "milk" :
+				order = new Milk(order);
+				break;
+
+			case "jasmine" :
+				order = new Jasmine(order);
+				break;
+
+			case "whip" :
+				i++;
+				order = new WhipCream(order);
+				break;
+
+			default :
+				System.out.println("Illegal input: " + orderMessage[i]);
+				System.exit(0);
+			}
+		}
+	}
+	
+	public void getDescription(Beverage order){
+		/**
+		 * How do I get the description of each order instead of doing this
+		 * stupid thing forever (except for printing the args)?
+		 */
+		if (order instanceof BeverageWithIngredient){
+			((BeverageWithIngredient) order).getDescription();
+		} 
+		else if (order instanceof Espresso) {
+			((Espresso) order).getDescription();
+		} 
+		else if (order instanceof HouseBlend){
+			((HouseBlend) order).getDescription();
+		} 
+		else if (order instanceof Decaf){
+			((Decaf) order).getDescription();
+		} 
+		else if (order instanceof RedTea){
+			((RedTea) order).getDescription();
+		} 
+		else if (order instanceof GreenTea){
+			((GreenTea) order).getDiscription();
+		} 
+		else {
+			((WhiteTea) order).getDescription();
+		}
+	}
+}
+
+
